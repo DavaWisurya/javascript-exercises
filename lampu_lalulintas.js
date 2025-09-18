9dbc754f40116b11216f031831ec0e1fddf56640
DavaWisurya <pixelaraa@gmail.com>
2025-09-18 12:20:58 +0700

    tugas javascript

---
+++ lampu_lalulintas.js
@@ -0,0 +1,14 @@
+
+
+let diulang = 'y'
+
+do {
+    let warna = prompt('Warna: ')
+    if (warna === "merah") {
+        console.log('berhenti')
+     } else if (warna === "kuning") {
+       console.log('bersiap')
+     }else
+        console.log('salah warna')        
+    diulang = prompt('Diulang (y/n) ? ')
+}while(diulang == 'y')
